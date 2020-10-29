b6cbcf74df19a4a1049cac62749191fa728b27c5
Неустроев Николай Иванович <nn@ikfia.ysn.ru>
2020-10-29 15:23:32 +0100

    add 2.pl

--- 2.pl
+++ 2.pl
@@ -23,21 +23,30 @@ my $client = $server->accept();
 $client->setsockopt(IPPROTO_TCP, TCP_NODELAY, 1);
 
 # 4
-print $client "HTTP/1.1 200 OK\r\n";
-print $client "Content-Type: text/html; charset=utf-8\r\n";
-print $client "Transfer-Encoding: chunked\r\n";
-print $client "Connection: keep-alive\r\n";
-print $client "\r\n";
+send_raw( $client, "HTTP/1.1 200 OK\r\n",
+                   "Content-Type: text/html; charset=utf-8\r\n",
+                   "Transfer-Encoding: chunked\r\n",
+                   "Connection: keep-alive\r\n",
+                   "\r\n" );
 
 # 5
-print $client "B\r\n"; # B - это 11 в шестнадцатиричном формате
-print $client "hello world\r\n";
+send_raw( $client, "B\r\n", "hello world\r\n" );
 
 # 6
-print $client "0\r\n";
-print $client "\r\n";
+send_raw( $client, "0\r\n", "\r\n" );
 
 # 7
 shutdown($client, 2);
 close($client);
+
+
+sub send_raw {
+  my $socket = shift;
+
+  foreach ( @_ ) {
+    print $socket $_;
+  }
+
+  return;
+}
 
